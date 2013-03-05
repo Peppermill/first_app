9efd04f43893ab1e40f3a33a04719bc5bb1744d3
Peppermill <reidaloud@gmail.com>
2013-03-04 20:23:48 -0500

    Initial RoR commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '310c13294234b7a33951ee8b41970fca38eb4b59ee136b360ae63b514381607e89b88bf8fec2aff726cd32de8adf37a13dd5eb8bc63712976de78dab52bf4833'
